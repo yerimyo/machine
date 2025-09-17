5907670fc44a0fbb2f1da95ad2aef6739561129f
Yerim Jo <yelim12337@gmail.com>
2025-09-17 13:53:12 +0900

    0917 13:53

--- 02-2.py
+++ 02-2.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 fish_length = [25.4, 26.3, 26.5, 29.0, 29.0, 29.7, 29.7, 30.0, 30.0, 30.7, 31.0, 31.0, 
                 31.5, 32.0, 32.0, 32.0, 33.0, 33.0, 33.5, 33.5, 34.0, 34.0, 34.5, 35.0, 
                 35.0, 35.0, 35.0, 36.0, 36.0, 37.0, 38.5, 38.5, 39.5, 41.0, 41.0, 9.8, 
@@ -9,12 +7,103 @@ fish_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 50
                 700.0, 725.0, 720.0, 714.0, 850.0, 1000.0, 920.0, 955.0, 925.0, 975.0, 950.0, 6.7, 
                 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
 
+import numpy as np
+
 np.column_stack(([1,2,3], [4,5,6]))
 
 fish_data = np.column_stack((fish_length, fish_weight))
 print(fish_data[:5])
 
-print(np.ones(5))
-
 fish_target = np.concatenate((np.ones(35), np.zeros(14)))
 print(fish_target)
+
+from sklearn.model_selection import train_test_split
+
+train_input, test_input, train_target, test_target = train_test_split(fish_data, fish_target, random_state=42)
+
+print(train_input.shape, test_input.shape)
+print(train_target.shape, test_target.shape)
+
+print(test_target)
+train_input, test_input, train_target, test_target = train_test_split(fish_data, fish_target, stratify=fish_target, random_state=42)
+print(test_target)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+kn = KNeighborsClassifier()
+kn.fit(train_input, train_target)
+kn.score(test_input, test_target)
+
+print(kn.predict([[25, 150]]))
+
+import matplotlib.pyplot as plt
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+distances, indexes = kn.kneighbors([[25, 150]])
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes,0], train_input[indexes,1], marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+print(train_input[indexes])
+
+print(train_target[indexes])
+
+print(distances)
+
+plt.scatter(train_input[:,0], train_input[:,1])
+plt.scatter(25, 150, marker='^')
+plt.scatter(train_input[indexes,0], train_input[indexes,1], marker='D')
+plt.xlim((0, 1000))
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+mean = np.mean(train_input, axis=0)
+std = np.std(train_input, axis=0)
+
+print(mean, std)
+
+train_scaled = (train_input - mean) / std
+
+
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(25, 150, marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+new = ([25, 150] - mean) / std
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1], marker='^')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
+
+kn.fit(train_scaled, train_target)
+
+test_scaled = (test_input - mean) / std
+
+kn.score(test_scaled, test_target)
+
+print(kn.predict([new]))
+
+distances, indexes = kn.kneighbors([new])
+
+plt.scatter(train_scaled[:,0], train_scaled[:,1])
+plt.scatter(new[0], new[1], marker='^')
+plt.scatter(train_scaled[indexes,0], train_scaled[indexes,1], marker='D')
+plt.xlabel('length')
+plt.ylabel('weight')
+plt.show()
