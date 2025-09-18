e9dc20b3a5832ae29fccc9e3d6c10e78b73844d7
Yerim Jo <yelim12337@gmail.com>
2025-09-19 00:13:36 +0900

    0919 12:14 확인문제 추가

--- 03-1.py
+++ 03-1.py
@@ -71,4 +71,24 @@ print(knr.score(train_input, train_target))
 print(knr.score(test_input, test_target))
 
 
+# 확인문제
+# k-최근접 이웃 회귀 객체를 만듭니다
+knr = KNeighborsRegressor()
+# 5에서 45까지 x 좌표를 만듭니다
+x = np.arange(5, 45).reshape(-1, 1)
 
+# n = 1, 5, 10일 때 예측 결과를 그래프로 그립니다
+for n in [1, 5, 10]:
+    # 모델을 훈련합니다
+    knr.n_neighbors = n
+    knr.fit(train_input, train_target)
+    # 지정한 범위 x에 대한 예측을 구합니다
+    prediction = knr.predict(x)
+
+    # 훈련 세트와 함께 그래프로 그립니다
+    plt.scatter(train_input, train_target)
+    plt.plot(x, prediction)
+    plt.title('n_neighbors = {}'.format(n))
+    plt.xlabel('length')
+    plt.ylabel('weight')
+    plt.show()
