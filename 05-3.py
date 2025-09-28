87a04fe2cdc1b6478fb884347d74175964d76e0f
Yerim Jo <yelim12337@gmail.com>
2025-09-28 10:48:54 +0900

    0928

--- 05-3.py
+++ 05-3.py
@@ -30,3 +30,45 @@ print(np.mean(scores['train_score']), np.mean(scores['test_score']))
 
 et.fit(train_input, train_target)
 print(et.feature_importances_)
+
+# 그레이디언트 부스팅
+from sklearn.ensemble import GradientBoostingClassifier
+gb = GradientBoostingClassifier(random_state=42)
+scores = cross_validate(gb, train_input, train_target, 
+                        return_train_score=True, n_jobs=-1)
+print(np.mean(scores['train_score']), np.mean(scores['test_score']))
+
+gb = GradientBoostingClassifier(n_estimators=500, learning_rate=0.2, random_state=42)
+scores = cross_validate(gb, train_input, train_target, 
+                        return_train_score=True, n_jobs=-1)
+print(np.mean(scores['train_score']), np.mean(scores['test_score']))
+
+gb.fit(train_input, train_target)
+print(gb.feature_importances_)
+
+# 히스토그램 기반 그레이디언트 부스팅
+from sklearn.ensemble import HistGradientBoostingClassifier
+hgb = HistGradientBoostingClassifier(random_state=42)
+scores = cross_validate(hgb, train_input, train_target, 
+                        return_train_score=True)
+print(np.mean(scores['train_score']), np.mean(scores['test_score']))
+
+from sklearn.inspection import permutation_importance
+
+hgb.fit(train_input, train_target)
+result = permutation_importance(hgb, train_input, train_target, 
+                                n_repeats=10, random_state=42, n_jobs=-1)
+print(result.importances_mean)
+
+result = permutation_importance(hgb, test_input, test_target, 
+                                 n_repeats=10, random_state=42, n_jobs=-1)
+print(result.importances_mean)
+
+hgb.score(test_input, test_target)
+
+from xgboost import XGBClassifier
+xgb = XGBClassifier(tree_method='hist', random_state=42)
+scores = cross_validate(xgb, train_input, train_target,
+                        return_train_score=True, n_jobs=-1)
+print(np.mean(scores['train_score']), np.mean(scores['test_score']))
+
