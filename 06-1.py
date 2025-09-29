1ab3fcf06efc4f55fc8f4d8bea669867d5ee41a5
Yerim Jo <yelim12337@gmail.com>
2025-09-30 03:20:16 +0900

    0930

--- 06-1.py
+++ 06-1.py
@@ -1,1 +1,25 @@
 # 군집 알고리즘
+
+# 과일 사진 데이터 준비하기
+import os
+os.system("wget https://bit.ly/fruits_300_data -O fruits_300.npy")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fruits = np.load('fruits_300.npy')
+
+print(fruits.shape)
+
+print(fruits[0, 0, :])
+
+plt.imshow(fruits[0], cmap='gray')
+plt.show()
+
+plt.imshow(fruits[0], cmap='gray_r')
+plt.show()
+
+fig, axs = plt.subplots(1, 2)
+axs[0].imshow(fruits[100], cmap='gray_r')
+axs[1].imshow(fruits[200], cmap='gray_r')
+plt.show()
