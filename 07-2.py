6d90a1084341f8c800f4848649917d613a46c238
Yerim Jo <yelim12337@gmail.com>
2025-10-07 21:43:33 +0900

    1007 21:43

--- 07-2.py
+++ 07-2.py
@@ -3,3 +3,41 @@
 from tensorflow import keras
 (train_input, train_target), (test_input, test_target) =\
     keras.datasets.fashion_mnist.load_data()
+
+from sklearn.model_selection import train_test_split
+
+train_scaled = train_input / 255.0
+train_scaled = train_scaled.reshape(-1, 28*28)
+train_scaled, val_scaled, train_target, val_target = train_test_split(
+    train_scaled, train_target, test_size=0.2, random_state=42
+)
+
+inputs = keras.layers.Input(shape=(784,))
+dense1 = keras.layers.Dense(100, activation='sigmoid')
+dense2 = keras.layers.Dense(10, activation='softmax')
+
+# 심층 신경망 만들기
+model = keras.Sequential([inputs, dense1, dense2])
+
+model.summary()
+
+# 층을 추가하는 다른 방법
+model = keras.Sequential([
+    keras.layers.Input(shape=(784,)),
+    keras.layers.Dense(100, activation='sigmoid', name='은닉층'),
+    keras.layers.Dense(10, activation='softmax', name='출력층')
+    ], name='패션 MNIST 모델')
+
+model.summary()
+
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=(784,)))
+model.add(keras.layers.Dense(100, activation='sigmoid'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+
+# 렐루 함수
