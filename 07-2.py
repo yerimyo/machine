b478b51a6ea8075f86c7d4612c45451d375de251
Yerim Jo <yelim12337@gmail.com>
2025-10-08 00:36:02 +0900

    1008 00:36

--- 07-2.py
+++ 07-2.py
@@ -41,3 +41,35 @@ model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(train_scaled, train_target, epochs=5)
 
 # 렐루 함수
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=(28,28)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.summary()
+
+(train_input, train_target), (test_input, test_target) =\
+    keras.datasets.fashion_mnist.load_data()
+train_scaled = train_input / 255.0
+train_scaled, val_scaled, train_target, val_target = train_test_split(
+    train_scaled, train_target, test_size=0.2, random_state=42
+)
+
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+
+model.evaluate(val_scaled, val_target)
+
+# 옵티마이저
+model = keras.Sequential()
+model.add(keras.layers.Input(shape=(28,28)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(100, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(train_scaled, train_target, epochs=5)
+
+model.evaluate(val_scaled, val_target)
