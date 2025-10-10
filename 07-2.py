79c7588678d27372289063de3f103c28f7d2b053
Yerim Jo <yelim12337@gmail.com>
2025-10-10 19:42:09 +0900

    1010 19:42

--- 07-2.py
+++ 07-2.py
@@ -62,6 +62,20 @@ model.fit(train_scaled, train_target, epochs=5)
 model.evaluate(val_scaled, val_target)
 
 # 옵티마이저
+# sgd = keras.optimizers.SGD()
+# model.compile(optimizer=sgd, loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+# sgd = keras.optimizers.SGD(learning_rate=0.1)
+# sgd = keras.optimizers.SGD(momentum=0.9, nesterov=True)
+
+# adagrad = keras.optimizers.Adagrad()
+# model.compile(optimizer=adagrad, loss='sparse_categorical_crossentropy',
+#               metirics=['accuracy'])
+
+# rmsprop = keras.optimizers.RMSprop()
+# model.compile(optimizer=rmsprop, loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+
 model = keras.Sequential()
 model.add(keras.layers.Input(shape=(28,28)))
 model.add(keras.layers.Flatten())
@@ -73,3 +87,77 @@ model.compile(optimizer='adam', loss='sparse_categorical_crossentropy',
 model.fit(train_scaled, train_target, epochs=5)
 
 model.evaluate(val_scaled, val_target)
+
+# 파이토치 버전
+from torchvision.datasets import FashionMNIST
+
+fm_train = FashionMNIST(root='.', train=True, download=True)
+fm_test = FashionMNIST(root='.', train=False, download=True)
+
+print(type(fm_train.data))
+
+print(fm_train.data.shape, fm_test.data.shape)
+
+print(fm_train.targets.shape, fm_test.targets.shape)
+
+train_input = fm_train.data
+train_target = fm_train.targets
+
+train_scaled = train_input / 255.0
+
+from sklearn.model_selection import train_test_split
+
+train_scaled, val_scaled, train_target, val_target = train_test_split(
+    train_scaled, train_target, test_size=0.2, random_state=42)
+
+print(train_scaled.shape, val_scaled.shape)
+
+import torch.nn as nn
+
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(784, 100),
+    nn.ReLU(),
+    nn.Linear(100, 10)
+)
+
+from torchinfo import summary
+
+summary(model, input_size=(32, 28, 28))
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+epochs= 5
+batches = int(len(train_scaled)/32)
+for epoch in range(epochs):
+    model.train()
+    train_loss = 0
+    for i in range(batches):
+        inputs = train_scaled[i*32:(i+1)*32].to(device)
+        targets = train_target[i*32:(i+1)*32].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+    print(f"에포크:{epoch + 1}, 손실:{train_loss/batches:.4f}")
+
+model.eval()
+with torch.no_grad():
+    val_scaled = val_scaled.to(device)
+    val_target = val_target.to(device)
+    outputs = model(val_scaled)
+    predicts = torch.argmax(outputs, 1)
+    corrects = (predicts == val_target).sum().item()
+
+accuracy = corrects / len(val_target)
+print(f"검증 정확도: {accuracy:.4f}")
