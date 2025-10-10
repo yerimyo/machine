4dbdf6470ce232fbbf17ffe09ee8128de15fc203
Yerim Jo <yelim12337@gmail.com>
2025-10-11 07:59:26 +0900

    1011 07:59

--- 07-3.py
+++ 07-3.py
@@ -6,5 +6,17 @@ from sklearn.model_selection import train_test_split
     keras.datasets.fashion_mnist.load_data()
 train_scaled = train_input / 255.0
 train_scaled, val_scaled, train_target, val_target = train_test_split(
-    train_scaled, train_target, test_size=0.2, random_state=42
-)
+    train_scaled, train_target, test_size=0.2, random_state=42)
+
+def model_fn(a_layer=None):
+    model = keras.Sequential()
+    model.add(keras.layers.Input(shape=(28,28)))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(100, activation='relu'))
+    if a_layer:
+        model.add(a_layer)
+    model.add(keras.layers.Dense(10, activation='softmax'))
+    return model
+
+model = model_fn()
+model.summary()
