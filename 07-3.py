89e8685bb23b496ff5fbf5d150393836d8a7bb3b
Yerim Jo <yelim12337@gmail.com>
2025-10-12 16:35:06 +0900

    1012 16:35

--- 07-3.py
+++ 07-3.py
@@ -20,3 +20,121 @@ def model_fn(a_layer=None):
 
 model = model_fn()
 model.summary()
+
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=5, verbose=0)
+
+print(history.history.keys())
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+plt.plot(history.history['accuracy'])
+plt.xlabel('epoch')
+plt.ylabel('accuracy')
+plt.show()
+
+model = model_fn()
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=20, verbose=0)
+plt.plot(history.history['loss'])
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.show()
+
+# 검증 손실
+model = model_fn()
+model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=20, verbose=0,
+                    validation_data=(val_scaled, val_target))
+
+print(history.history.keys())
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+
+model = model_fn()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=20, verbose=0,
+                    validation_data=(val_scaled, val_target))
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+# 드롭아웃
+model = model_fn(keras.layers.Dropout(0.3))
+model.summary()
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=20, verbose=0,
+                    validation_data=(val_scaled, val_target))
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+# 모델 저장과 복원
+model = model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_scaled, train_target, epochs=11, verbose=0,
+                    validation_data=(val_scaled, val_target))
+
+model.save('model-whole.keras')
+
+model.save_weights('model.weights.h5')
+
+model =model_fn(keras.layers.Dropout(0.3))
+model.load_weights('model.weights.h5')
+
+import numpy as np
+
+val_labels = np.argmax(model.predict(val_scaled), axis=-1)
+print(np.mean(val_labels == val_target))
+
+model = keras.models.load_model('model-whole.keras')
+model.evaluate(val_scaled, val_target)
+
+# 콜백
+model = model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+checkpoint_cb = keras.callbacks.ModelCheckpoint('best-model.keras', save_best_only=True)
+model.fit(train_scaled, train_target, epochs=20, verbose=0,
+          validation_data=(val_scaled, val_target),
+          callbacks=[checkpoint_cb])
+
+model = keras.models.load_model('best-model.keras')
+model.evaluate(val_scaled, val_target)
+
+
+model = model_fn(keras.layers.Dropout(0.3))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+checkpoint_cb = keras.callbacks.ModelCheckpoint('best-model.keras', save_best_only=True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=2, restore_best_weights=True)
+histroy = model.fit(train_scaled, train_target, epochs=20, verbose=0,
+          validation_data=(val_scaled, val_target),
+          callbacks=[checkpoint_cb, early_stopping_cb])
+
+print(early_stopping_cb.stopped_epoch)
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+model.evaluate(val_scaled, val_target)
