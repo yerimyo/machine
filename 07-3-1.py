d582b57a40d90b4153a62b024f2d94da2fc4cc06
Yerim Jo <yelim12337@gmail.com>
2025-10-12 17:15:05 +0900

    1012 17:15

---
+++ 07-3-1.py
@@ -0,0 +1,98 @@
+# 파이토치 버전
+from torchvision.datasets import FashionMNIST
+
+fm_train = FashionMNIST(root='.', train=True, download=True)
+fm_test = FashionMNIST(root='.', train=False, download=True)
+
+train_input = fm_train.data
+train_target = fm_train.targets
+train_scaled = train_input /255.0
+
+from sklearn.model_selection import train_test_split
+
+train_scaled, val_scaled, train_target, val_target = train_test_split(
+    train_scaled, train_target, test_size=0.2, random_state=42)
+
+import torch.nn as nn
+
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(784, 100),
+    nn.ReLU(),
+    nn.Dropout(0.3),
+    nn.Linear(100, 10)
+)
+
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters())
+
+train_hist = []
+val_hist = []
+patience = 2
+best_loss = -1
+early_stopping_counter = 0
+
+epochs= 20
+batches = int(len(train_scaled)/32)
+for epoch in range(epochs):
+    model.train()
+    train_loss = 0
+    for i in range(batches):
+        inputs = train_scaled[i*32:(i+1)*32].to(device)
+        targets = train_target[i*32:(i+1)*32].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+    model.eval()
+    val_loss = 0
+    with torch.no_grad():
+        val_scaled = val_scaled.to(device)
+        val_target = val_target.to(device)
+        outputs = model(val_scaled)
+        loss = criterion(outputs, val_target)
+        val_loss = loss.item()
+    train_hist.append(train_loss/batches)
+    val_hist.append(val_loss)
+    print(f"에포크:{epoch+1}",
+          f"훈련 손실:{train_loss/batches:.4f}, 검증 손실:[val_loss:.4f]")
+    if best_loss == -1 or val_loss < best_loss:
+        best_loss = val_loss
+        early_stopping_counter = 0
+        torch.save(model.state_dict(), 'best_model.pt')
+    else:
+        early_stopping_counter += 1
+        if early_stopping_counter >= patience:
+            print(f"{epoch+1}번째 에포크에서 조기 종료되었습니다.")
+            break
+
+import matplotlib.pyplot as plt
+
+plt.plot(train_hist, label='train')
+plt.plot(val_hist, label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+model.load_state_dict(torch.load('best_model.pt', weights_only=True))
+
+model.eval()
+with torch.no_grad():
+    val_scaled = val_scaled.to(device)
+    val_target = val_target.to(device)
+    outputs = model(val_scaled)
+    predicts = torch.argmax(outputs, 1)
+    corrects = (predicts == val_target).sum().item()
+
+accuracy = corrects / len(val_target)
+print(f"검증 정확도: {accuracy:.4f}")
