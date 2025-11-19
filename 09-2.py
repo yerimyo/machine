add3b567343647cd27dd4a02a42a8a3109f7b333
Yerim Jo <yelim12337@gmail.com>
2025-11-19 13:59:39 +0900

    1119 13:59

---
+++ 09-2.py
@@ -0,0 +1,3 @@
+from keras.datasets import imdb
+
+(train_input, train_target), (test_input)
---
+++ 09-3.py
@@ -0,0 +1,70 @@
+# LSTM 신경망 훈련하기
+from keras.datasets import imdb
+from sklearn.model_selection import train_test_split
+
+(train_input, train_target), (test_input, test_target) = imdb.load_data(num_words=500)
+train_input, val_input, train_target, val_target = train_test_split(
+    train_input, train_target, test_size=0.2, random_state=42)
+
+from keras.preprocessing.sequence import pad_sequences
+
+train_seq = pad_sequences(train_input, maxlen=100)
+val_seq = pad_sequences(val_input, maxlen=100)
+
+import keras
+
+model_lstm = keras.Sequential()
+model_lstm.add(keras.layers.Input(shape=(100,)))
+model_lstm.add(keras.layers.Embedding(500, 16))
+model_lstm.add(keras.layers.LSTM(8))
+model_lstm.add(keras.layers.Dense(1, activation='sigmooid'))
+
+model_lstm.summary()
+
+model_lstm.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+checkpoint_cb = keras.callbacks.ModelCheckpoint('best-lstm-model.keras', save_best_only=True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+history = model_lstm.fit(train_seq, train_target, epochs=100, batch_size=64,
+                         validation_data=(val_seq, val_target),
+                         callbacks=[checkpoint_cb, early_stopping_cb])
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legent()
+plt.show()
+
+# 순환층에 드롭아웃 적용하기
+model_dropout = keras.Sequential()
+model_dropout.add(keras.layers.Input(shape=(100,)))
+model_dropout.add(keras.layers.Embedding(500, 16))
+model_dropout.add(keras.layers.LSTM(8, dropout=0.2))
+model_dropout.add(keras.layers.Dense(1, activation='sigmoid'))
+
+model_dropout.compile(optimizer='adam', loss='binary_crossentropy',
+                      metrics=['accuracy'])
+checkpoint_cb = keras.callbacks.ModelCheckpoint('best-dropout-model.keras',
+                                                save_best_only=True)
+early_stopping_cb = keras.callbacks.EarlyStopping(patience=3, restore_best_weights=True)
+history = model_dropout.fit(train_seq, train_target, epochs=100, batch_size=64,
+                            validation_data=(val_seq, val_target),
+                            callbacks=[checkpoint_cb, early_stopping_cb])
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['vall_loss'], label='val')
+plt.xlabel('epoch')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+# 2개의 층을 연결하기
+odel_2lstm = keras.Sequential()
+model_2lstm.add(keras.layers.Input(shape=(100,)))
+model_2lstm.add(keras.layers.Embedding(500, 16))
+model_2lstm.add(keras.layers.LSTM(8, dropout=0.2, return_sequences=True))
+model_2lstm.add(keras.layers.LSTM(8, dropout=0.2))
+model_2lstm.add(keras.layers.Dense(1, activation='sigmoid'))
+model_2lstm.summary()
